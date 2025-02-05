989e0a15f5fc90465708d1f73288eb6835b2e204
Breno Sobral <ssobralbreno@gmail.com>
2025-02-04 21:58:43 -0300

    feat: 🎸 add auto docs generate and cache setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,10 @@ import { LoggingInterceptor } from './common/logging/logging.interceptor'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 import { LoggingModule } from './common/logging'
 import { HealthModule } from './health/health.module'
+import * as redisStore from 'cache-manager-redis-store'
 import { ThrottlerModule } from '@nestjs/throttler'
 import { CustomThrottlerGuard } from './common/guards/throttler.guard'
+import { CacheModule, CacheModuleOptions } from '@nestjs/cache-manager'
 
 @Module({
   imports: [
@@ -14,6 +16,11 @@ import { CustomThrottlerGuard } from './common/guards/throttler.guard'
       isGlobal: true,
     }),
     ThrottlerModule.forRoot(),
+    CacheModule.register(<CacheModuleOptions>{
+      isGlobal: true,
+      store: redisStore,
+      ttl: 3600 * 1000,
+    }),
     LoggingModule,
     HealthModule,
   ],
