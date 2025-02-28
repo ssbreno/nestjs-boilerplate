972d44fb2357752428c529f1b22588c23744cac3
Breno Sobral <ssobralbreno@gmail.com>
2025-02-28 20:48:55 -0300

    feat: 🎸 architechture structure init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,46 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core'
+import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler'
+import { CacheModule } from '@nestjs/cache-manager'
+
 import { LoggingInterceptor } from './common/logging/logging.interceptor'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
-import { LoggingModule } from './common/logging'
-import { HealthModule } from './health/health.module'
-import * as redisStore from 'cache-manager-redis-store'
-import { ThrottlerModule } from '@nestjs/throttler'
 import { CustomThrottlerGuard } from './common/guards/throttler.guard'
-import { CacheModule, CacheModuleOptions } from '@nestjs/cache-manager'
+import { LoggingModule } from './common/logging'
+import { PrismaModule } from './infraestructure/prisma/prisma.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { UsersModule } from './modules/users/users.module'
+import { QueueModule } from './modules/queue/queue.module'
+import { redisConfig } from './config/redis.config'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ThrottlerModule.forRoot(),
-    CacheModule.register(<CacheModuleOptions>{
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService): Promise<ThrottlerModuleOptions> => ({
+        throttlers: [
+          {
+            ttl: config.get<number>('THROTTLE_TTL') || 60,
+            limit: config.get<number>('THROTTLE_LIMIT') || 100,
+          },
+        ],
+      }),
+    }),
+    CacheModule.registerAsync({
       isGlobal: true,
-      store: redisStore,
-      ttl: 3600 * 1000,
+      useFactory: redisConfig,
+      inject: [ConfigService],
     }),
     LoggingModule,
-    HealthModule,
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    QueueModule,
   ],
   providers: [
     {
